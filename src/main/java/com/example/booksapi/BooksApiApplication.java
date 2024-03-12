b50a8aff4b8697f97d261a63b649fe4e8d786ac0
Muhammad Danish Roslan <muhammaddanishroslan@gmail.com>
2024-03-12 15:48:58 +0800

    Add specific repository package location at @EnableJpaRepositories

--- src/main/java/com/example/booksapi/BooksApiApplication.java
+++ src/main/java/com/example/booksapi/BooksApiApplication.java
@@ -2,12 +2,13 @@ package com.example.booksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.booksapi.repository")
 public class BooksApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApiApplication.class, args);
 	}
-
 }
